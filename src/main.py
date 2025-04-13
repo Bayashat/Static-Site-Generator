8da93030a759eb27cf04489391793b880d49f5e9
Bayashat <baiashat2002@gmail.com>
2025-04-13 15:42:02 +0500

    feat: Implement PROD ready build

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from block_markdown import markdown_to_html_node
 from copy_static import copy_files_recursive
@@ -7,12 +8,13 @@ from gen_content import generate_page, generate_pages_recursive
 from htmlnode import HTMLNode
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    base_path = sys.argv[1] if sys.argv else "/"
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -21,7 +23,9 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(
+        dir_path_content, template_path, dir_path_public, base_path
+    )
 
 
 main()
